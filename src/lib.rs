8eb04bf65fc98469d3ec03bc287502cdb28c8ac2
jinpu <jinpu@LAPTOP-J1SUAON8>
2018-01-07 16:36:19 +0900

    学習の処理を実装。学習時にNNの評価結果と教師データを渡して学習済みのNNオブジェクトを返すタプルを返そうとしていたが、トレイトオブジェクトのライフタイムの関係でどうやっても無理で断念。 やむなく出力のスナップショットを返すようにした。 ※出力を返したNNと異なる状態のNNで学習を実行すると正しく動かないと思われる。

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,32 @@ pub mod function;
 pub mod error;
 pub mod persistence;
 
-use function::activation::ActivateF;
-use function::loss::LossFunction;
-use function::optimizer::Optimizer;
+use function::activation::*;
+use function::loss::*;
+use function::optimizer::*;
 use error::*;
 use std::error::Error;
 
-pub struct NN<'a,O,E,C> where O: Optimizer, E: LossFunction, C: Fn() -> O {
+pub struct NN<'a,O,E,F> where O: Optimizer, E: LossFunction, F: Fn() -> O {
 	model:&'a NNModel<'a>,
-	optimizer_creator:C,
+	optimizer_creator:F,
 	lossf:E,
 }
 
-impl<'a,O,E,C> NN<'a,O,E,C> where O: Optimizer, E: LossFunction, C: Fn() -> O {
-	pub fn new(model:&'a NNModel<'a>,optimizer_creator:C,lossf:E) -> NN<'a,O,E,C> {
+impl<'a,O,E,F> NN<'a,O,E,F> where O: Optimizer, E: LossFunction, F: Fn() -> O {
+	pub fn new(model:&'a NNModel<'a>,optimizer_creator:F,lossf:E) -> NN<'a,O,E,F> {
 		NN {
 			model:model,
 			optimizer_creator:optimizer_creator,
 			lossf:lossf,
 		}
+	}
+
+	pub fn save<P,ERR>(&mut self,mut persistence:P) -> Result<(),PersistenceError<ERR>>
+		where P: Persistence<ERR>, ERR: Error, PersistenceError<ERR>: From<ERR> {
+		persistence.save(&self.model.layers)?;
+
+		Ok(())
 	}
 }
 pub struct NNModel<'a> {
@@ -31,6 +38,13 @@ impl<'a> NNModel<'a> {
 	pub fn load<I,E>(mut reader:I) -> Result<NNModel<'a>, E>
 		where I: ModelInputReader<E>, E: Error, StartupError<E>: From<E> {
 		reader.read_model()
+	}
+
+	fn new(units:Vec<(usize,Option<&'a ActivateF>)>,layers:Vec<Vec<Vec<f64>>>) -> NNModel<'a> {
+		NNModel {
+			units:units,
+			layers:layers,
+		}
 	}
 
 	pub fn with_bias_and_unit_initializer<I,F,E>(
@@ -202,11 +216,126 @@ impl<'a> NNModel<'a> {
 		})
 	}
 
-	pub fn save<P,E>(&mut self,mut persistence:P) -> Result<(),PersistenceError<E>>
-		where P: Persistence<E>, E: Error, PersistenceError<E>: From<E> {
-		persistence.save(&self.layers)?;
+	pub fn promise_of_learn<O,E,F>(&self,input:Vec<f64>) ->
+		Result<SnapShot,InvalidStateError> {
 
-		Ok(())
+		if input.len() != self.units.len() {
+			return Err(InvalidStateError::InvalidInput(String::from(
+				"The inputs to the input layer is invalid (the count of inputs must be the count of units)")));
+		}
+
+		let mut o:Vec<Vec<f64>> = Vec::with_capacity(self.units.len());
+		let mut u:Vec<Vec<f64>> = Vec::with_capacity(self.units.len());
+
+		u.push(Vec::new());
+
+		let mut oi:Vec<f64> = Vec::with_capacity(self.units[0].0 + 1);
+
+		oi.push(1f64);
+
+		for i in input {
+			oi.push(i);
+		}
+
+		o.push(oi);
+
+		u.push(Vec::with_capacity(self.units[1].0 + 1));
+
+		u[1].resize(self.units[1].0 + 1, 0f64);
+
+		//for(int k=1, K=units[1].size+1; k < K; k++)
+		for (u,(&oi,li)) in u[1].iter_mut().zip(o[0].iter().zip(&self.layers[0])) {
+			//for(int j=0, J=units[0].size+1; j < J; j++)
+			for ui in li {
+				*u = *u + oi * ui;
+				//u[1][k] += (o[0][j] * layers[0][j][k-1]);
+			}
+		}
+
+		o.push(Vec::with_capacity(self.units[1].0 + 1));
+		o[1].resize(self.units[1].0 + 1, 0f64);
+
+		let f:&ActivateF = match self.units[1].1 {
+			Some(f) => f,
+			None => {
+				return Err(InvalidStateError::InvalidInput(String::from(
+							"Reference to the activation function object is not set."
+						)));
+			}
+		};
+
+		o[1][0] = 1f64;
+
+		//for(int j=1, J = units[1].size+1; j < J; j++)
+		for (oi,&ui) in o[1].iter_mut().zip(u[1].iter()) {
+			//o[1][j] = f.apply(u[1][j]);
+			*oi = f.apply(ui,&u[1]);
+		}
+
+		//for(int l=1, L=units.length - 1; l < L; l++)
+		for l in 0..self.units.len() - 1 {
+			//final int ll = l + 1;
+			let ll = l + 1;
+			let ul:Vec<f64> = Vec::with_capacity(self.units[ll].0 + 1);
+			u.push(ul);
+			//u[ll] = new double[units[ll].size+1];
+			//f = units[l].f;
+			let f:&ActivateF = match self.units[l].1 {
+				Some(f) => f,
+				None => {
+					return Err(InvalidStateError::InvalidInput(String::from(
+								"Reference to the activation function object is not set."
+							)));
+				}
+			};
+
+			//o[ll] = new double[units[ll].size+1];
+			let ol:Vec<f64> = Vec::with_capacity(self.units[ll].0 + 1);
+			o.push(ol);
+
+			let layer = &self.layers[l];
+
+			//for(int k=1, K = units[ll].size+1; k < K; k++)
+			for (ui,k) in u[ll].iter_mut().zip(0..self.units[ll].0) {
+				//for(int j=0, J=units[l].size+1; j < J; j++)
+				for (&oi,j) in o[l].iter().zip(0..self.units[l].0 + 1) {
+					//u[ll][k] += o[l][j] * layers[l][j][k-1];
+					*ui = *ui + oi * layer[j][k];
+				}
+
+				//o[ll][k] = f.apply(*u[ll][k]);
+				//o[ll][0] = 1.0;
+			}
+
+			for (oi,&ui) in o[ll].iter_mut().zip(u[ll].iter()) {
+				*oi = f.apply(ui, &u[ll]);
+			}
+		}
+
+		//double[] r = new double[units[units.length-1].size];
+		let mut r:Vec<f64> = Vec::with_capacity(self.units[self.units.len()-1].0);
+
+		//for(int k=1, K = units[units.length-1].size+1, l=units.length-1; k < K; k++)
+		for &oi in o[self.units.len()-1].iter() {
+			//r[k-1] = o[l][k];
+			r.push(oi);
+		}
+
+		Ok(SnapShot::new(r,o,u))
+	}
+}
+pub struct SnapShot {
+	r:Vec<f64>,
+	o:Vec<Vec<f64>>,
+	u:Vec<Vec<f64>>,
+}
+impl SnapShot {
+	pub fn new(r:Vec<f64>,o:Vec<Vec<f64>>,u:Vec<Vec<f64>>) -> SnapShot {
+		SnapShot {
+			r:r,
+			o:o,
+			u:u,
+		}
 	}
 }
 pub trait InputReader<E> {
