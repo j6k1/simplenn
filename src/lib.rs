c01ff97f519bbbca92c4d3c7265ab517a83362ba
jinpu <jinpu@DESKTOP-3N3HFJE>
2021-12-11 22:26:01 +0900

    バグ修正 入力層のバイアス相当の値に活性化関数を適用していなかったので修正

--- src/lib.rs
+++ src/lib.rs
@@ -1115,8 +1115,7 @@ impl<T> NNModel<T> where T: UnitValue<T> {
 
 		o[1][0] = T::bias();
 
-		for (oi,&ui) in o[1].iter_mut().skip(1).zip(u[1].iter().skip(1)) {
-			// インデックス0はバイアスのユニットなので一つ右にずらす
+		for (oi,&ui) in o[1].iter_mut().zip(u[1].iter()) {
 			*oi = f.apply(ui,&u[1]);
 		}
 
